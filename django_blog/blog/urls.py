900c51b0862bd8ee8e2c4d7fbe151cbff2c8408c
Ivy Kemunto <ivy.kemunto.masese@gmail.com>
2026-03-01 20:52:58 +0300

    Add login.css and register.css static files, update templates and urls with include import

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,7 +6,7 @@ urlpatterns = [
     # Home
     path('', views.HomeView.as_view(), name='home'),
 
-    # Authentication
+    # Authentication URLs using include for organization
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
