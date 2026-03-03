cbf7afe5abe7b60c9a122fc179a2c9b910409b43
Ivy Kemunto <ivy.kemunto.masese@gmail.com>
2026-03-03 18:10:02 +0300

    Add PostgreSQL database config with PORT

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = "social_media_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "social_media_db",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
